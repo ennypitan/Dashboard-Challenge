586bde182fba20c968686d9558f589ca43f25c07
Ennypitan Adekoya <ennypitan@gmail.com>
2024-01-24 02:04:46 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,19 @@ function App() {
         <article>
           <div className="flex-row">
             <div className="flex-container upper-dashboard">
-              <div className="sales-trend">Sales Trend</div>
+              <div className="sales-trend">
+                <div className="headings flex-row">
+                  <div className="trend">Sales Trends</div>
+                  <div className="sort">
+                    <label for="sorting">Sort by:</label>
+                    <select id="sorting" name="sort">
+                      <option value="weekly">Weekly</option>
+                      <option value="monthly">Monthly</option>
+                      <option value="yearly">Yearly</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
               <div className="order-summary">Order Summary</div>
             </div>
           </div>
