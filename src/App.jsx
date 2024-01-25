fda115daf59e53d74c92e5f1d6ed89818f6c3ca3
Ennypitan Adekoya <ennypitan@gmail.com>
2024-01-25 15:24:33 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,19 @@ function App() {
           </div>
           <div className="flex-row">
             <div className="flex-container lower-dashboard">
-              <div className="last-orders">Last Orders</div>
+              <div className="last-orders">
+                <div className="heading flex-row">
+                  <div className="l-order">Last Orders</div>
+                  <div className="see-all">See All</div>
+                </div>
+                <div className="sub-heading flex-row">
+                  <div className="name">Name</div>
+                  <div className="t-date">Date</div>
+                  <div className="amount">Amount</div>
+                  <div className="status">Status</div>
+                  <div className="invoice">Invoice</div>
+                </div>
+              </div>
               <div className="top-platforms">Top Platforms</div>
             </div>
           </div>
