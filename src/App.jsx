3f6fe7464689648836f8ada6a141ace6331d6cbe
Ennypitan Adekoya <ennypitan@gmail.com>
2024-02-15 00:00:29 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import Header from "./components/Header/Header.jsx";
-import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import TrendPage from "./components/TrendPage/TrendPage.jsx";
-import "./index.css";
 // import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <div className="wrapper">
-        <header>
-          <Header />
-        </header>
-        <article>
-          <Dashboard />
-        </article>
-        <aside>
-          <Sidebar />
-        </aside>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Header from "./components/Header/Header.jsx";
+// import Dashboard from "./components/Dashboard/Dashboard.jsx";
+// import TrendPage from "./components/TrendPage/TrendPage.jsx";
+import "./../index.css";
 
 function Layout() {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Sidebar />
+      <div className="wrapper">
+        <header>
+          <Header />
+        </header>
+        <article>
+          <Outlet />
+        </article>
+        <aside>
+          <Sidebar />
+        </aside>
+      </div>
     </>
   );
 }
