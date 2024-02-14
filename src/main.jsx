5f44d77e3b92ff1e532cab6b55769f77a1db074e
Ennypitan Adekoya <ennypitan@gmail.com>
2024-02-14 23:47:38 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import TrendPage from "./components/TrendPage/TrendPage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Dashboard />} />
+      <Route path="trendpage" element={<TrendPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
